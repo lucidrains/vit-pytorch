a6c085a2df7d28a996d1287bd0b254ee8efcd9f6
Phil Wang <lucidrains@gmail.com>
2021-07-02 15:48:48 -0700

    0.20.0 for cct

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vit-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '0.19.6',
+  version = '0.20.0',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   author = 'Phil Wang',

e5324242be61bcbf433e129e914aa4b4fa1a79a0
Phil Wang <lucidrains@gmail.com>
2021-08-05 12:55:48 -0700

    fix wrong norm in nest

--- vit_pytorch/nest.py
+++ vit_pytorch/nest.py
@@ -10,9 +10,19 @@ from einops.layers.torch import Rearrange, Reduce
 def cast_tuple(val, depth):
     return val if isinstance(val, tuple) else ((val,) * depth)
 
-LayerNorm = partial(nn.InstanceNorm2d, affine = True)
-
 # classes
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        std = torch.var(x, dim = 1, unbiased = False, keepdim = True).sqrt()
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) / (std + self.eps) * self.g + self.b
 
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):

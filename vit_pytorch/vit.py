1467f5e823874e5ba3abab494ca4c921c1b8ff03
Yonghye Kwon <developer.0hye@gmail.com>
2021-08-30 18:06:23 +0900

    Cleanup Attention Class
    Cleanup Attention Class

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -50,9 +50,8 @@ class Attention(nn.Module):
         ) if project_out else nn.Identity()
 
     def forward(self, x):
-        b, n, _, h = *x.shape, self.heads
         qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
 
         dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
 

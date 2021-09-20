8e20587462cf467f7a87a959c55fe892eda9558b
Anirudh Thatipelli <thatipellianirudh@gmail.com>
2021-09-20 11:23:37 +0530

    Update vit.py
    **image_height, image_width, patch_height, patch_width** variables should be initialized as 
    
    **image_height = image_width = pair(image_size)
      patch_height = patch_width = pair(patch_size)
    **

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -79,8 +79,8 @@ class Transformer(nn.Module):
 class ViT(nn.Module):
     def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
         super().__init__()
-        image_height, image_width = pair(image_size)
-        patch_height, patch_width = pair(patch_size)
+        image_height = image_width = pair(image_size)
+        patch_height = patch_width = pair(patch_size)
 
         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 

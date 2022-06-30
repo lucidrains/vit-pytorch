2fa2b62def9274f604a3ea0cd7edd7b1ddeacf23
Phil Wang <lucidrains@gmail.com>
2022-06-30 08:11:17 -0700

    slightly more clear of einops rearrange for cls token, for https://github.com/lucidrains/vit-pytorch/issues/224

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -114,7 +114,7 @@ class ViT(nn.Module):
         x = self.to_patch_embedding(img)
         b, n, _ = x.shape
 
-        cls_tokens = repeat(self.cls_token, '1 n d -> b n d', b = b)
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
         x = torch.cat((cls_tokens, x), dim=1)
         x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)

ad4ca1977541609d78e88e8175f9d103020d50d2
Phil Wang <lucidrains@gmail.com>
2023-05-08 09:34:14 -0700

    enforce latest einops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vit-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '1.2.0',
+  version = '1.2.1',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   long_description_content_type = 'text/markdown',
@@ -16,7 +16,7 @@ setup(
     'image recognition'
   ],
   install_requires=[
-    'einops>=0.6.0',
+    'einops>=0.6.1',
     'torch>=1.10',
     'torchvision'
   ],

ca7d7e39e3b50c487f9513cbb50ac19c1b9b500e
lucidrains <lucidrains@gmail.com>
2023-10-06 10:22:55 -0700

    improvise a max vit with register tokens

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vit-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '1.5.0',
+  version = '1.5.1',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   long_description_content_type = 'text/markdown',

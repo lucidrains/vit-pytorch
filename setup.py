fb37832f66cdb0c8086ed819e0324d757e46f121
lucidrains <lucidrains@gmail.com>
2023-10-13 09:15:00 -0700

    document

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vit-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '1.5.3',
+  version = '1.6.0',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   long_description_content_type = 'text/markdown',

53fe345e8585f0d539bba73754789e051c703e93
lucidrains <lucidrains@gmail.com>
2023-10-19 18:16:43 -0700

    no longer needed with einops 0.7

--- vit_pytorch/__init__.py
+++ vit_pytorch/__init__.py
@@ -1,10 +1,3 @@
-import torch
-from packaging import version
-
-if version.parse(torch.__version__) >= version.parse('2.0.0'):
-    from einops._torch_specific import allow_ops_in_compiled_graph
-    allow_ops_in_compiled_graph()
-
 from vit_pytorch.vit import ViT
 from vit_pytorch.simple_vit import SimpleViT
 

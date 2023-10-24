66cfecf928a3ef19af8fe88373fafdbad7f63872
Artem Lukin <artyom.lukin98@gmail.com>
2023-10-24 21:41:32 +0200

    Fix typo in LayerNorm

--- vit_pytorch/vit_1d.py
+++ vit_pytorch/vit_1d.py
@@ -10,7 +10,7 @@ class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Layernorm(dim),
+            nn.LayerNorm(dim),
             nn.Linear(dim, hidden_dim),
             nn.GELU(),
             nn.Dropout(dropout),

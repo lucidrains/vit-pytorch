5e808f48d1995a190ec963871129caf7018e9777
Phil Wang <lucidrains@gmail.com>
2024-08-21 07:23:21 -0700

    3d version of navit nested tensor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
   name = 'vit-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '1.7.7',
+  version = '1.7.10',
   license='MIT',
   description = 'Vision Transformer (ViT) - Pytorch',
   long_description=long_description,
